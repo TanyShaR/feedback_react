e5ae81f75da13b5138bbd7ce7aed6e971cee4fd5
TanjaRetter <88773218+TanyShaR@users.noreply.github.com>
2025-07-10 23:32:27 +0200

    update

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,22 +1,64 @@
 import S from "./FeedbackForm.module.css";
 
-export function FeedbackForm(props) {
+export function FeedbackForm({ onChange, onSubmit }) {
+    const onFieldChange = (event) => {
+        const { target } = event;
+        const { name, value } = target;
+        // console.log(name, event);
+        // console.log(name, value);
+        onChange?.(name, value);
+    };
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const { target } = event;
+
+        const formData = new FormData(target);
+        const data = Object.fromEntries(formData);
+        
+        //console.log(data);
+        onSabmit?.(data);
+    };
+      // const validateForm = (data) => {};
   return (
-    <div className={S.container}>
+    <form className={S.container} onSubmit={onFormSubmit}>
       <div className={S.group}>
-        <input type="text" placeholder="Your Name" className={S.input} />
+        <input 
+        type="text" 
+        placeholder="Your Name" 
+        className={S.input} 
+        name="username"
+        onChange={onFieldChange}
+        required
+        />
       </div>
       <div className={S.group}>
-        <input type="email" placeholder="Email" className={S.input} />
+        <input 
+        type="email" 
+        placeholder="Email" 
+        className={S.input}
+        name="email"
+        onChange={onFieldChange}
+        required
+         />
       </div>
       <div className={S.group}>
-        <input placeholder="Your Message" className={S.input} />
+        <textarea 
+        placeholder="Your Message" 
+        className={S.input} 
+        name="message"
+        onChange={onFieldChange}
+        required
+        />
       </div>
       <div className={S.group}>
         <label className={S.group}>
-          <input type="checkbox" />I accept the data processing policy
+          <input type="checkbox" name="policy" required />
+          I accept the data processing policy
         </label>
       </div>
-    </div>
+      <div className={S.group}>
+        <button>Send</button>
+      </div>
+    </form>
   );
 }
