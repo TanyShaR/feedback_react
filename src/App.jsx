c49494297cc8650db17a19be6c2a8329f794adcc
TanjaRetter <88773218+TanyShaR@users.noreply.github.com>
2025-07-11 12:26:22 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import { FeedbackForm } from "./components/FeedbackForm/FeedbackForm";
 import { Modal } from "./components/Modal/Modal";
 
 function App() {
+  const [form, setForm] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const onSubmit = (data) => {
+      setForm(data);
+      setShowModal(true);
+  };
   return (
     <>
-      <FeedbackForm />
-      <Modal title="Thank you for writing!" show>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illum nemo, eius sequi recusandae molestiae vitae dolorem rerum quo quae ex commodi, asperiores facilis nihil fuga dolor expedita explicabo quas!
+      <FeedbackForm onSubmit={onSubmit} />
+      {form && (
+      <Modal title="Thank you for writing!" show={showModal}>
+        {form.username}, Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illum nemo, eius sequi recusandae molestiae vitae dolorem rerum quo quae ex commodi, asperiores facilis nihil fuga dolor expedita explicabo quas! {form.email}
       </Modal>
+      )}
     </>
   );
 }
